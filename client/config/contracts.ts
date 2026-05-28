090e8eaf365db70409e55904cd03e4bb121f9772
user1938521573 <1145032086666756-user1938521573@noreply.coze.cn>
2026-05-28 23:02:03 +0800

    auto saved your changes before restore
    Coze-Commit-Type: auto
    Coze-User-ID: 1145032086666756

--- client/config/contracts.ts
+++ client/config/contracts.ts
@@ -6,25 +6,48 @@
 
 // 主合约地址
 export const CONTRACT_ADDRESSES = {
+  // 核心合约
   DQPROJECT: {
-    address: '0xb55693887D6e7E902DACa763904093151aE660D1',
-    name: 'DQProject'
+    address: '0xa85eE02619F0eD39D7CB9E76ed417005E16c7D97',
+    name: 'DQMCore'
   },
   DQSTAKE: {
-    address: '0xA24a2abda5C973dB31f04C43E6a52DcE7F1c2132',
-    name: 'DQStake'
+    address: '0xEE8327F146b984bD3a57dfbac6B9f9cfdDa82536',
+    name: 'DQMiningStakeCore'
   },
+  // 代币合约
   DQTOKEN: {
-    address: '0x3bc8C7b5b4Ec8798Fa9d8D177Ca0FCC317a24989',
-    name: 'DQToken'
+    address: '0x9CC5c62345BCf133754126007b8Dff1a546E130F',
+    name: 'DQT'
   },
   DQCARD: {
-    address: '0x4006e454DB83836ed7A261ba3E894Fe3E3D6F7A9',
+    address: '0x1d6a57144F263B7c8BD4f4bbB9A9516aabd756A4',
     name: 'DQCard'
   },
   SOL: {
     address: '0x570A5D26f7765Ecb712C0924E4De545B89fD43dF',
     name: 'SOL'
+  },
+  // 其他合约
+  DQPAIR: {
+    address: '0xe7f2cf83b91d6738813d9d5bfce209ea03c06979',
+    name: 'DQPAIR'
+  },
+  DQLPMigrator: {
+    address: '0x8eB742d12488f6689831599e8B12d66090BFE69c',
+    name: 'DQLPMigrator'
+  },
+  DQMAdmin: {
+    address: '0xcB09EF06E60d4F69AF0d2AA63bd74b34C8Fcfe4F',
+    name: 'DQMAdmin'
+  },
+  DQMiningStakeMine: {
+    address: '0xbefe3DE3837eaC1EfbB7bB5A3240250B031304Fc',
+    name: 'DQMiningStakeMine'
+  },
+  DQMiningStakeVault: {
+    address: '0xf0abfAf1135E07C0bb378b3d1B652D258Ef7cdBF',
+    name: 'DQMiningStakeVault'
   }
 };
 
